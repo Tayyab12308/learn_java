8b3574d8772fed5079e134d19941d9f445785245
Tayyab Iqbal <tayyab12308@gmail.com>
2019-11-25 16:02:37 -0500

    practice ternary

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -9,5 +9,24 @@ public class Hello {
         int total = num + num2 + num3;
         int lastTotal = total - 1000;
         System.out.println(lastTotal);
+
+        byte firstNum = 10;
+        short secondNum = 100;
+        int thirdNum = 400;
+        long fourthNum = 50000 + (10 * (firstNum + secondNum + thirdNum));
+        System.out.println(fourthNum);
+
+        int pounds = 10;
+        double poundsToKilos = 0.45359237;
+        System.out.println(pounds * poundsToKilos);
+
+
+        double firstVar = 20.00;
+        double secondVar = 80.00;
+        double thirdVar = (firstVar + secondVar) * 100;
+        double remainder = thirdVar % 40.00;
+        boolean noRemainder = remainder == 0 ? true : false;
+        System.out.println(noRemainder);
+        if (noRemainder) System.out.println("Got no remainder");
     }
 }
