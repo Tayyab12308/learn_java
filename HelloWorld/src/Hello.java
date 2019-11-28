e4684c0d1a8fd0400a990e16aaeaf1f327804818
Tayyab Iqbal <tayyab12308@gmail.com>
2019-11-28 15:21:22 -0500

    method overloading practice

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -78,10 +78,26 @@ public class Hello {
         return calcFeetAndInchesToCentimeters(feet, extraInches);
     }
 
+    public static String getDurationString(int minutes, int seconds) {
+        if (minutes < 0 || seconds < 0 || seconds > 59) return "Invalid Value";
+        int hours = minutes / 60;
+        int remainingMinutes = minutes % 60;
+        return hours + "h " + remainingMinutes + "m " + seconds + "s ";
+    }
+
+    public static String getDurationString(int seconds) {
+        if (seconds < 0) return "Invalid Value";
+        int minutes = seconds / 60;
+        int remainingSeconds = seconds % 60;
+        return  getDurationString(minutes, remainingSeconds);
+    }
+
     public static void main(String[] args) {
         List<Integer> unsorted = new ArrayList<>(Arrays.asList(1, 3, 2, 4, 6, 5));
         System.out.println(merge_sort(unsorted));
 
         System.out.println(calcFeetAndInchesToCentimeters(-2));
+        System.out.println(getDurationString(65, 45));
+        System.out.println(getDurationString(300));
     }
 }
